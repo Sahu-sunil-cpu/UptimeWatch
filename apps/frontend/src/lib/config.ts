08529b7ac289af9084f9064c5ebbb232fba6e092
sunil sahu <sunilsahu5886@gmail.com>
2025-11-15 15:43:59 +0530

    ci/cd test

--- apps/frontend/src/lib/config.ts
+++ apps/frontend/src/lib/config.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = "http://51.20.12.164:3001"
-export const AUTHORIZATION = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MmE3ZTFhMS0yY2Q4LTQwMzktYjJmYy05ODY3YTNmMTExMWQiLCJpYXQiOjE3NjMwMjY4Mjl9.vtrOS4L_FXEyXIg3BYzGuw5glUQfNc_z2amK0pUHia0'
+export const AUTHORIZATION = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MmE3ZTFhMS0yY2Q4LTQwMzktYjJmYy05ODY3YTNmMTExMWQiLCJpYXQiOjE3NjMyMDE0NzV9.YOit-zD_y6-Mbm8diVwjYTdp0_sE2USjQf0KWN0xE18'
 
 
 // sudo docker run -d  -p 6379:6379   redis:7-alpine   --protected-mode no   --bind 0.0.0.0
